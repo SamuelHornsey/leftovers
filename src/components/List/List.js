dc8ec37e7fcc602d9604ede7d26705b4cce7458e
Samuel Hornsey <hornseysam498@gmail.com>
2020-01-11 13:31:20 +1100

    Feature/ci builds (#3)
    * added travis file
    
    * updated build script and ci build
    
    * fixed package.json
    
    * fixed linting

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,11 +3,9 @@ import { h } from 'preact';
 import AuthBase from '../AuthBase/AuthBase';
 import Item from './Item';
 
-import { getList } from '../../services/list';
-
 import './List.scss';
 
-import timer from '../../assets/sand-clock.png';
+// import timer from "../../assets/sand-clock.png";
 
 export default class List extends AuthBase {
   constructor(props) {
@@ -21,58 +19,6 @@ export default class List extends AuthBase {
 
   componentWillMount() {
     this.checkAuth();
-
-    getList()
-      .then(recipes => {
-        const loading = false;
-        this.setState({
-          recipes,
-          loading
-        });
-      });
-  }
-
-  _renderRecipe() {
-    const { recipes, loading, error } = this.state;
-    const tiles = [];
-
-    if (loading) {
-      return (
-        <div class="Search__loading">
-          <img src={timer} alt="Loading" />
-        </div>
-      );
-    }
-
-    if (error) {
-      return (
-        <div class="Search__none">
-          <h4>Oops. Something Went Wrong...</h4>
-        </div>
-      );
-    }
-
-    if (recipes.length === 0) {
-      return (
-        <div class="Search__none">
-          <h4>No Results...</h4>
-        </div>
-      );
-    }
-
-    recipes.map((recipe, i) => {
-      tiles.push(
-        <Recipe
-          id={recipe.id}
-          missedIngredientCount={recipe.missedIngredientCount}
-          title={recipe.title}
-          image={recipe.image}
-          key={i}
-        />
-      );
-    });
-
-    return tiles;
   }
 
   render() {
@@ -80,9 +26,7 @@ export default class List extends AuthBase {
       <div class="Search">
         <h1 class="Search__title">List</h1>
 
-        {/* <Item title="Apples" status={true} /> */}
-
-        {/* {this._renderRecipe()} */}
+        <Item title="Apples" status />
       </div>
     );
   }
