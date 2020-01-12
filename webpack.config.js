c889179e3316f46946215a6334b6bdfaf625ea85
Samuel Hornsey <hornseysam498@gmail.com>
2020-01-12 22:17:32 +1100

    added GA tag

--- webpack.config.js
+++ webpack.config.js
@@ -3,77 +3,92 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxPlugin = require("workbox-webpack-plugin");
 const path = require("path");
 
-const devMode = process.env.NODE_ENV === "production";
+const GA = `
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129566070-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-module.exports = {
-  // entry file - starting point for the app
-  entry: "./src/index.js",
+  gtag('config', 'UA-129566070-3');
+</script>
+`;
 
-  // where to dump the output of a production build
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: devMode ? "bundle.js" : "bundle.[hash].js"
-  },
+module.exports = (env, argv) => {
+  const devMode = argv.mode !== "production";
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/i,
-        loader: "babel-loader"
-      },
+  return {
+    // entry file - starting point for the app
+    entry: "./src/index.js",
 
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader"
-          }
-        ]
-      },
+    // where to dump the output of a production build
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: devMode ? "bundle.js" : "bundle.[hash].js"
+    },
 
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: "css-loader",
-            options: { minimize: true }
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      }
-    ]
-  },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/i,
+          loader: "babel-loader"
+        },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      favicon: "src/assets/favicon.png"
-    }),
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader"
+            }
+          ]
+        },
 
-    new MiniCssExtractPlugin({
-      filename: devMode ? "main.css" : "main.[hash].css",
-      chunkFilename: devMode ? "main.[id].css" : "main.[id].[hash].css"
-    }),
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              loader: "css-loader",
+              options: { minimize: true }
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
+        }
+      ]
+    },
 
-    // new WorkboxPlugin.GenerateSW()
-  ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+        favicon: "src/assets/favicon.png",
+        GA: devMode ? "" : GA
+      }),
 
-  // enable Source Maps
-  devtool: "source-map",
+      new MiniCssExtractPlugin({
+        filename: devMode ? "main.css" : "main.[hash].css",
+        chunkFilename: devMode ? "main.[id].css" : "main.[id].[hash].css"
+      })
 
-  devServer: {
-    // serve up any static files from src/
-    contentBase: path.join(__dirname, "src"),
+      // new WorkboxPlugin.GenerateSW()
+    ],
 
-    // enable gzip compression:
-    compress: true,
+    // enable Source Maps
+    devtool: "source-map",
 
-    // enable pushState() routing, as used by preact-router et al:
-    historyApiFallback: true
-  }
+    devServer: {
+      // serve up any static files from src/
+      contentBase: path.join(__dirname, "src"),
+
+      // enable gzip compression:
+      compress: true,
+
+      // enable pushState() routing, as used by preact-router et al:
+      historyApiFallback: true
+    }
+  };
 };
