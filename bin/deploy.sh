42af367e8df7fe49f09e93022bef46e189785eb2
Samuel Hornsey <hornseysam498@gmail.com>
2022-03-10 23:14:36 +1100

    removed bin scripts

--- bin/deploy.sh
+++
@@ -1,8 +0,0 @@
-# install dependencies
-npm install -g s3-deploy
-npm install
-
-# deployment
-npm run clean
-npm run build
-s3-deploy './dist/**' --cwd './dist/' --region $AWS_REGION --bucket $AWS_S3_BUCKET
